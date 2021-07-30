1eb3d0d17b2943d4e6406ce27580c8e62ab7763c
GarrickWong <62346820+GarrickWong@users.noreply.github.com>
2021-07-30 10:01:21 -0700

    Add files via upload

--- imagePassCode.js
+++ imagePassCode.js
@@ -2,9 +2,10 @@
 //GLOBAL VARIABLES
 //==============================================================================================================
 
-var textpassword = "W2nD5!fm3?q";
+var numSet = new Array();
 
 var password = "";
+var temp;
 var passwordSet = new Array();
 var passwordSetCount = 0;
 
@@ -15,6 +16,10 @@ var inputPasswordSetCount = 0;
 var c, nx, ny, dx, dy/*, picmaxx, picmaxy*/;
 var picx;
 var picy;
+
+var az = "1!2@3#4$5%6^7&8*9?AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYy";
+
+
 var paper;
 var imageDir = ".."
 
@@ -27,7 +32,12 @@ var tileFiles;
 var tileImages;
 var tileImageSet;
 
+
 var numClicks = 5;
+
+var tileset = null;
+var tileset2 = null;
+var tileset3 = null;
 
 var button0;
 var button1;
@@ -45,61 +55,118 @@ var allopacity = 0.0;
 
 var showp = false;
 var choosingPic = false;
+var choosingPwd = false;
+var mode;
 
 var div0;
 var div1;
-//var div2;
-//var div3;
-//var div4;
-//var div5;
-//var div6;
-//var div7;
-//var div8;
-//var div9;
-//var div10;
-//var div11;
-//var div12;
-//var div13;
 
 function genPassword() {
+  mode = "create";
+  showp = true;
+  passwordSetCount = 0;
+  password = "";
+  passwordSet = new Array();
+  while (passwordSetCount < numClicks) {
+    temp = [getRandom(0,9),getRandom(0,7)];
+    for (var i = 0; i < passwordSetCount; i++) {
+      if(passwordSet[i] == temp) {
+        temp = null;
+        break;
+      }
+    }
+    if(temp != null) {
+      passwordSet[passwordSetCount] = temp;
+      numSet[passwordSetCount] = [getRandom(0,67 - passwordSet[passwordSetCount][0]), getRandom(0,67 - passwordSet[passwordSetCount][1])]
+      passwordSetCount ++;
+    }
+  }
+  passwordSet.sort();
+  for (var i = 0; i < passwordSetCount; i++)   password += az.substr(passwordSet[i][0]+numSet[i][0],1) + az.substr(passwordSet[i][1]+numSet[i][1],1);
+  if (tileset != null)   tileset = paper.clear();
+  if (tileset2 != null)   tileset2 = paper.clear();
+  if (tileset3 != null)   tileset3 = paper.clear();
   createCanvas();
+  updateCanvas();
   button0 = document.createElement("button");
-  button0.innerHTML += "Show Password";
+  button0.innerHTML += "Hide Password";
   button1 = document.createElement("button");
-  button1.innerHTML += "Check Password";
-  button2 = document.createElement("button");
-  button2.innerHTML += "Confirm";
-  div0 = document.getElementById("sub");
+  button1.innerHTML += "Confirm";
+  div0 = document.getElementById("button_div");
   div0.appendChild(button0);
   div0.appendChild(button1);
-  div0.appendChild(button2);
   div0.style.display = "initial";
   document.getElementById("main").style.display = "none";
-  button0.setAttribute("onclick","placeholder()");
-  button1.setAttribute("onclick","placeholder()");
-  button2.setAttribute("onclick","returnMain()");
+  button0.setAttribute("onclick","hidePassword()");
+  button1.setAttribute("onclick","returnMain()");
 }
+
 function releasePassword() {
-  div0 = document.createElement("div");
-  div1 = document.getElementById("password");
-  div0.innerHTML += textpassword;
-  if(div1.childNodes.length === 0) {
-    div1.style.display = "initial";
-    div1.appendChild(div0);
-    setTimeout(function(){div1.removeChild(div1.childNodes[0]);
-      div1.style.display = "none"; }, 5000);
+  mode = "enter";
+  showp = false;
+  createCanvas();
+  button1 = document.createElement("button");
+  button1.innerHTML += "Go Back";
+  div0 = document.getElementById("button_div");
+  div0.appendChild(button1);
+  div0.style.display = "initial";
+  document.getElementById("main").style.display = "none";
+  button1.setAttribute("onclick","returnMain()");
+}
+
+function hidePassword() {
+  showp = false;
+  button0.innerHTML = "Show Password";
+  button0.setAttribute("onclick", "showPassword()");
+  updateCanvas();
+}
+
+function showPassword() {
+  showp = true;
+  button0.innerHTML = "Hide Password";
+  button0.setAttribute("onclick", "hidePassword()");
+  updateCanvas();
+}
+
+function getRandom(min, max) {
+    var randomNum = Math.random() * (max-min);
+    return(Math.round(randomNum) + min);
+}
+
+function doClick(coords) {
+    this.ij = coords;
+    var current = this;
+    this.invoke = function () {
+
+	if (choosingPic) return;
+	if (inputPasswordSetCount >= numClicks) return;
+
+	i = current.ij[0];
+	j = current.ij[1];
+
+
+	inputPasswordSet[inputPasswordSetCount] = [i,j];
+	inputPasswordSetCount ++;
+
+	updateCanvas();
+	updateCanvas2();
+    }
+
   }
-}
-function placeholder() {
-  alert("Not implemented");
-}
+
+  function inInputPassword(coords) {
+      for (var k=0; k<inputPasswordSetCount; k++) {
+  	if (inputPasswordSet[k][0]==coords[0] && inputPasswordSet[k][1]==coords[1]) return true;
+      }
+      return false;
+  }
 
 function createCanvas() {
   div1 = document.getElementById("canvas_container");
   div1.style.display = "initial";
   //creates canvas to draw on
-  paper = Raphael(div1, 100000, 100000);
-  nx = 8
+  paper = Raphael(div1, 2000, 2000);
+  nx = 10
   ny = 8
   dx = 1152/nx;
   dy = 648/ny;
@@ -181,13 +248,13 @@ function createCanvas() {
       tilesquares[[i,j]] = paper.rect(i*dx+3,j*dy+3,dx-6,dy-6);
       tilesquares2[[i,j]] = paper.rect(i*dx+3,j*dy+3,dx-6,dy-6);
       tilesquares3[[i,j]] = paper.rect(i*dx,j*dy,dx,dy);
-  //    if (mode=="create") {
+      if (mode=="create") {
         tileDigits[[i,j]] = "";
         tileNumbers[[i,j]] = paper.text(i*dx+0.5*dx, j*dx+0.5*dx, "");
         tileNumbers[[i,j]].attr({'font-size': 32, fill: 'yellow'});
         tileNumbers2[[i,j]] = paper.text(i*dx+0.5*dx, j*dx+0.5*dx, "");
         tileNumbers2[[i,j]].attr({'font-size': 28, fill: 'black'});
-    /*  }
+      }
 
       else {
         r = getRandom(0,9);
@@ -197,7 +264,7 @@ function createCanvas() {
         tileNumbers2[[i,j]] = paper.text(i*dx+0.5*dx, j*dx+0.5*dx, r);
         tileNumbers2[[i,j]].attr({'font-size': 28, fill: 'black'});
       }
-*/
+
       tilesquares2[[i,j]].node.onclick = new doClick([i,j]).invoke;
       tilesquares3[[i,j]].node.onclick = new doClick([i,j]).invoke;
       tileset.push(tilesquares[[i,j]]);
@@ -207,10 +274,6 @@ function createCanvas() {
       numberSet.push(tileNumbers2[[i,j]]);
     }
   }
-
-  //putting the object recog items into their grid
-
-  //if(div1.childNodes.length != 0) {
 
     tileset.show();
     tileset2.show();
@@ -225,52 +288,167 @@ function createCanvas() {
     tileset3.toFront();
 
 
-//  }
+    tileset3.attr({fill: allsq, stroke: allb1, 'fill-opacity': 0.0, 'stroke-opacity': 1.0, 'stroke-width': 0.0001});
 
-/*  if (shuffleButton) {
-    sButton = paper.rect(picminx+400, picy+5, 100, 20, 10);
-    sButton.attr({fill: "lightgrey"});
+    numberSet.hide();
+    updateCanvas();
+}
 
-    sText = paper.text(picminx+445, picy+15, "Shuffle");
-    sText.attr({'font-size': 14});
-    sText.node.style.cursor = "default";
+function enterPassword() {
+  div0 = document.createElement("div");
+  div1 = document.getElementById("password");
+  inputPassword = "";
+  for (var i = 0; i < inputPasswordSetCount; i++)   inputPassword += az.substr(inputPasswordSet[i][0]+numSet[i][0],1) + az.substr(inputPasswordSet[i][1]+numSet[i][1],1);
 
 
-    sButton.node.onclick = function() {
-        recogShuffle(true);
+    inputPassword = "";
 
-        tileImageSet.show();
-        tileImageSet.toFront();
+    inputPasswordSet.sort();
 
-        updateCanvas();
+
+	for (var i = 0; i < inputPasswordSetCount; i++)   inputPassword += az.substr(inputPasswordSet[i][0]+numSet[i][0],1) + az.substr(inputPasswordSet[i][1]+numSet[i][1],1);
+
+    if (mode=="create") {
+	     if (inputPassword == password) {
+
+		div0.innerHTML = "Yay! You correctly entered your password!";
+
+
+	}
+	else { // inputPassword != password
+	    div0.innerHTML = "Oops, try again! ";
+
+		}
+
+  while(div1.childNodes.length != 0) {
+    div1.removeChild(div1.lastChild);
+  }
+  div1.style.display = "initial";
+  div1.appendChild(div0);
+	inputPasswordSet = Array();
+	inputPasswordSetCount = 0;
+	updateCanvas();
+
     }
 
-    sText.node.onclick = sButton.node.onclick;
+    else { //mode is enter
+	if (inputPassword == password) {
+   div0.innerHTML = inputPassword;
+   while(div1.childNodes.length != 0) {
+     div1.removeChild(div1.lastChild);
+   }
+   div1.style.display = "initial";
+   div1.appendChild(div0);
+
   }
-*/
+	else {
+     div0.innerHTML = "Password is incorrect";
+     while(div1.childNodes.length != 0) {
+       div1.removeChild(div1.lastChild);
+     }
+     div1.style.display = "initial";
+     div1.appendChild(div0);
 
-//  tileset3.attr({fill: allsq, stroke: allb1, 'fill-opacity': 0.0, 'stroke-opacity': 1.0, 'stroke-width': 0.0001});
-
-
-
-/*  if (!digitLogin) {
-    numberSet.hide();
   }
-  else numberSet.show();
-  */
+
+    }
+}
+
+
+function updateCanvas2() {
+
+    if (inputPasswordSetCount==numClicks) {
+	if (mode=="create" && choosingPwd) {
+	    $.prompt("Click 'Accept Password' to choose this password, or 'Clear' to select another. ");
+
+	}
+	else enterPassword();
+
+    }
+
+    updateCanvas();
+}
+
+function updateCanvas() {
+
+
+    if (mode=="create") {
+
+
+    console.log(picfile);
+    picture = paper.image(picfile, 1, 1, picx, picy);
+
+    if (showp) allopacity = 0.1;
+    else allopacity = 0.0;
+
+
+    if (choosingPic) {
+	    tileset.hide();
+    	tileset2.hide();
+      tileset3.hide();
+
+    }
+    else {
+	   tileset.show();
+	   tileset2.show();
+	   tileset3.show();
+    }
+
+    tileset.attr({fill: allsq, stroke: allb1, 'fill-opacity': allopacity, 'stroke-opacity': 1.0, 'stroke-width': 1});
+    tileset2.attr({fill: allsq, stroke: allb2, 'fill-opacity': allopacity, 'stroke-opacity': 1.0, 'stroke-width': .5});
+    tileset.toFront();
+
+    tileset2.toFront();
+    tileset3.toFront();
+    numberSet.toFront();
+
+
+    if (showp) {
+
+	numberSet.attr({text:""});
+
+	for (var i=0; i<passwordSetCount; i++) {
+	    tilesquares[passwordSet[i]].attr({fill: pwdsq, stroke: pwdb1, 'fill-opacity': 0.0, 'stroke-opacity': 1.0, 'stroke-width': 7});
+	    tilesquares2[passwordSet[i]].attr({fill: pwdsq, stroke: pwdb2, 'fill-opacity': 0.0, 'stroke-opacity': 1.0, 'stroke-width': 3});
+	    tileNumbers[passwordSet[i]].attr({text: i+1})
+	    tileNumbers2[passwordSet[i]].attr({text: i+1})
+	    tilesquares[passwordSet[i]].toFront();;
+	    tilesquares2[passwordSet[i]].toFront();
+	    tileNumbers[passwordSet[i]].toFront();
+	    tileNumbers2[passwordSet[i]].toFront();
+	    tilesquares3[passwordSet[i]].toFront();
+	}
+
+	  }
+  for (var i=0; i<inputPasswordSetCount; i++) {
+	    tilesquares[inputPasswordSet[i]].attr({fill: pwdsq, stroke: "blue", 'fill-opacity': 0.0, 'stroke-opacity': 1.0, 'stroke-width': 7});
+	    tilesquares2[inputPasswordSet[i]].attr({fill: pwdsq, stroke: "limegreen", 'fill-opacity': 0.0, 'stroke-opacity': 1.0, 'stroke-width': 3});
+
+	    tilesquares[inputPasswordSet[i]].toFront();
+	    tilesquares2[inputPasswordSet[i]].toFront();
+	    tileNumbers[inputPasswordSet[i]].toFront();
+	    tileNumbers2[inputPasswordSet[i]].toFront();
+	    tilesquares3[inputPasswordSet[i]].toFront();
+	}
+
 }
 
 
 function returnMain() {
-  div0 = document.getElementById("sub");
+  div0 = document.getElementById("button_div");
   div1 = document.getElementById("canvas_container");
+  div2 = document.getElementById("password");
   while (div0.hasChildNodes()) {
     div0.removeChild(div0.lastChild);
   }
   while (div1.hasChildNodes()) {
     div1.removeChild(div1.lastChild);
   }
+  while (div2.hasChildNodes()) {
+    div2.removeChild(div2.lastChild);
+  }
   div0.style.display = "none";
   div1.style.display = "none";
+  div2.style.display = "none";
   document.getElementById("main").style.display = "initial"
 }
